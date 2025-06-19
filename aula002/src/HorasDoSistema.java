6c8509f182a94d1b94095fc37604e1dda002968a
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-06-19 14:47:23 -0300

    test

--- aula002/src/HorasDoSistema.java
+++ aula002/src/HorasDoSistema.java
@@ -1,9 +1,17 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class HorasDoSistema{
     public static void main(String[] args) {
         Date relogio = new Date();
-        System.out.print("A hora do sistema é: ");
-        System.out.print(relogio.toString());
+        System.out.println("A hora do sistema é: " + relogio.toString());
+        //O código acima apresenta a data e a hora em um formato desorganizado
+
+        LocalTime agora = LocalTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String horaFormatada = agora.format(formato);
+        System.out.println("A hora agora é: " + horaFormatada);    
+        //O codigo acima apresenta a hora em um formato mais organizado
     }
 }
--- aula002/src/SystemHour.java
+++ aula002/src/SystemHour.java
@@ -1,19 +1,19 @@
+import java.util.Date;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class SystemHour{
-    // public static void main(String[] args) {
-    //     Date clock = new Date();
-    //     System.out.print("The system hour is: ");
-    //     System.out.print(clock.toString());
-    // }
-    //O código acima printa a data e a hora
-    public static void main(String[] args) {
+
+    public static void main(String[] args) { 
+
+        Date clock = new Date();
+        System.out.println("The system hour is: " + clock.toString());
+        //The above code displays the date and time in a disorganized format.
+
         LocalTime now = LocalTime.now();
         DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss");
         String formattedTime = now.format(format);
-
-    System.out.println("The hour now is " + formattedTime);
+        System.out.println("The hour now is " + formattedTime);    
+        //The above code displays the time in a organized format.
     }
-    //O código acima printa apenas a hora
 }
