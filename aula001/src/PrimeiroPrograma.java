5e88d2a98360397233ba951f2700aff1bc20616c
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-06-19 14:29:33 -0300

    test

--- aula001/src/PrimeiroPrograma.java
+++ aula001/src/PrimeiroPrograma.java
@@ -1,6 +1,6 @@
 public class PrimeiroPrograma {
 
     public static void main(String[] args){
-        System.out.println("Hello, World");    
+        System.out.println("Olá, Mundo!");    
  }
 }
