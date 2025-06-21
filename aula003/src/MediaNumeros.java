da14fc104259af80d3cdabfceffe9ed92bac45f4
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-06-21 00:16:01 -0300

    test

--- aula003/src/MediaNumeros.java
+++ aula003/src/MediaNumeros.java
@@ -8,15 +8,16 @@ public class MediaNumeros {
         MediaNumeros valorObjeto = new MediaNumeros();
         for (int i=1; i<=3; i++){
             double num = Math.random()*100;
-            System.out.println("Este é o número " +i+ " em formato double gerado aleatoriamente: " + num + "\n");
+            System.out.println("Este é o número " +i+ " em formato double gerado aleatoriamente: " + num);
             double numArredondado = Math.round(num * 100) / 100.0;
             System.out.println("Este é o número " +i+ " arredondado para o mais próximo dele: "+numArredondado+"\n");
             valorObjeto.soma += numArredondado;
             valorObjeto.nNumbers++;
         }
+        double somaArredondada = Math.round(valorObjeto.soma * 100) / 100.0;
         double media = valorObjeto.soma / valorObjeto.nNumbers;
         double mediaArredondado = Math.round(media * 100) / 100.0;
-        System.out.println("Essa é a soma dos valores: " + valorObjeto.soma);
+        System.out.println("Essa é a soma dos valores: " + somaArredondada);
         System.out.println("Essa é a média da soma dos valores " + mediaArredondado +"\n");
     }
 }
