5015ce8a9e19d0fd455b8406e26aa68ea3641bff
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-06-22 17:03:29 -0300

    test

--- aula004/src/Matriz2x2.java
+++ aula004/src/Matriz2x2.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class Matriz2x2 {
     
+    public static int[][] matrizMetodo(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o número de linhas");
+        int linhas = scanner.nextInt();
+        System.out.println("Digite o número de colunas");
+        int colunas = scanner.nextInt();
+        int[][] matrix = new int[linhas][colunas];
+        for(int i=0; i<linhas; i++){
+            for(int z=0; z<colunas; z++){
+            System.out.println("Digite o número para a posição [" + (i+1) + "][" + (z+1) + "]: ");
+                matrix[i][z] = scanner.nextInt();
+            }
+        }
+        scanner.close();
+        return matrix;
+    }
+    public static void imprimirMatriz (int[][] matriz){
+        System.out.println("\nMatriz Digitada:");
+        for(int i = 0; i< matriz.length; i++){
+            for(int j = 0; j< matriz[i].length; j++){
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+    int[][] resultado = matrizMetodo();
+    imprimirMatriz(resultado);
+    }
 }
