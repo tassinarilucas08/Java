ba58ffc8609c633f9faea266a60e2963b852c2d2
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-06-24 13:23:29 -0300

    test

---
+++ aula005/src/SomaMatriz.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SomaMatriz {
+
+    public static int[][] montaMatriz(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o número de linhas");
+        int linhas = scanner.nextInt();
+        System.out.println("Digite o número de colunas");
+        int colunas = scanner.nextInt();
+        int [][] matrix = new int [linhas][colunas];
+        for(int i=0; i<linhas; i++){
+            for(int z=0; z<colunas; z++){
+             System.out.println("Digite o número para a posição [" + (i+1) + "][" + (z+1) + "]: ");
+             matrix[i][z] = scanner.nextInt();
+            }
+        }
+        return matrix;
+     }
+    }
+
