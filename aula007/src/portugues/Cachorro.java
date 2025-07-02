d48c1cc01e56d94c3a669ecc00152bec7efbbc65
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-07-02 15:04:25 -0300

    test

---
+++ aula007/src/portugues/Cachorro.java
@@ -0,0 +1,11 @@
+package portugues;
+
+public class Cachorro {
+    String nome;
+    int idade;
+    String raca;
+    
+    public void Latir(){
+        System.out.println(nome + "diz: Au au!");
+    }
+}
---
+++ aula007/src/portugues/Main.java
@@ -0,0 +1,5 @@
+package portugues;
+
+public class Main {
+    
+}
