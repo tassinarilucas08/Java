9954cbd1db5f29ba3523b32662dd34b50dcee5a2
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-07-01 23:12:24 -0300

    test

--- aula006/src/Tabuada.java
+++ aula006/src/Tabuada.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Tabuada {
     public static void MontarTabuada(){
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número para descobrir sua tabuada do 1 ao 10");
+        int numberEscolhido = scanner.nextInt();
+        for(int i=1; i<=10; i++){
+            int calculo = numberEscolhido * i;
+            System.out.println(numberEscolhido+"X"+i + " = "+calculo);
+        }
+        scanner.close();
+    }
+    public static void main(String[] args) {
+        MontarTabuada();
     }
 }
