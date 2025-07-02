5353d0a09b262c4e1ae3eee9d8a7bc65c8411d8e
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-07-01 23:04:23 -0300

    test

---
+++ aula006/src/ContarVogais.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ContarVogais {
+    public static void metodoContarVogais(){
+        int numVogais = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha uma palavra e descubra quantas vogais tem");
+        String palavra = scanner.nextLine();
+        for(int i=0; i<palavra.length(); i++){
+            char letra = palavra.charAt(i);
+            char letraMaiuscula = Character.toUpperCase(letra);
+            if (letraMaiuscula == 'A' || 
+            letraMaiuscula == 'E' || 
+            letraMaiuscula == 'I' || 
+            letraMaiuscula == 'O' ||
+            letraMaiuscula == 'U') {
+                numVogais++;
+            }
+        }
+        System.out.println("A palavra tem " + numVogais +" vogais");
+        scanner.close();
+    }
+    public static void main(String[] args) {
+        metodoContarVogais();
+    }
+}
