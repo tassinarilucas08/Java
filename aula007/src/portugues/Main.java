c8d3ea8b2134fac1d4f64dc6a63465b71f072d14
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-07-04 00:00:09 -0300

    test

--- aula007/src/portugues/Main.java
+++ aula007/src/portugues/Main.java
@@ -6,14 +6,15 @@ public class Main {
         cachorro1.nome = "Dogao";
         cachorro1.idade = 3;
         cachorro1.raca = "Labrador";
-
         
         cachorro1.Latir();
+        cachorro1.Comer();
         
         Cachorro cachorro2 = new Cachorro();
         cachorro2.nome = "Dogona";
         cachorro2.idade = 2;
         cachorro2.raca = "Poodle";
         cachorro2.Latir();
+        cachorro2.Comer();
     }
 }
